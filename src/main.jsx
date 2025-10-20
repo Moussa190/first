0e73c64b55364d60b60c3646bf3e78578ed99aa5
MHD-HSN-TD <8883597!@mhd>
2025-10-20 17:06:44 +0300

    remove all imported css and js file in the index.html

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// 2. IMPORT VENDOR CSS FILES (Installed via npm/yarn)
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'aos/dist/aos.css';
+import 'boxicons/css/boxicons.min.css';
+
+
+// 6. IMPORT YOUR TEMPLATE'S MAIN JS LOGIC
+import '../public/assets/js/main.js';
+
+
+
+
 createRoot(document.getElementById('root')).render(
- 
+
     <App />
- 
+
 )
