ad84b7741a3bdef77a21da4674bb240b9fb55dcc
MHD-HSN-TD <8883597!@mhd>
2025-10-20 17:23:11 +0300

    remove css for, the main.js

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import './index.css'
 import App from './App.jsx'
 
 // 2. IMPORT VENDOR CSS FILES (Installed via npm/yarn)
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'aos/dist/aos.css';
-import 'boxicons/css/boxicons.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'aos/dist/aos.css';
+// import 'boxicons/css/boxicons.min.css';
 
 
 // 6. IMPORT YOUR TEMPLATE'S MAIN JS LOGIC
